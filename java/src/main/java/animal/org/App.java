8529e0f83ed9c367f75025ca9c00b825ca8f6229
dhrjkumar05 <dhe.kumar@cgi.com>
2017-04-04 17:28:00 +0530

    Add files via upload

---
+++ java/src/main/java/animal/org/App.java
@@ -0,0 +1,14 @@
+package animal.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class  
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+return 1;
+    }
+}
